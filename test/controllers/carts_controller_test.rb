50b9e2f2011564c760417541c55062e36777a837
JamesWdev <96626506+JamesWdev@users.noreply.github.com>
2022-09-22 09:22:21 -0400

    Add files via upload

---
+++ test/controllers/carts_controller_test.rb
@@ -0,0 +1,18 @@
+# require "test_helper"
+# require "rspec-rails"
+
+class CartsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+# My test
+
+# describe CartsController do
+#   describe "#destroy" do
+#     it "destroys all cart instances"
+#       carts-controller = CartsController.new
+#       expect(carts-controller.destroy).to eql(Cart.all.count = 0)
+#     end
+# end
